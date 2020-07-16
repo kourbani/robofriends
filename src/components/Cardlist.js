26b00d5cacc867bf787733f9abde454c63792489
kourbani <kanidoux@gmail.com>
2020-07-16 16:54:40 +0300

    fixing http and https content errors

---
+++ src/components/Cardlist.js
@@ -0,0 +1,24 @@
+import React, {Fragment} from "react";
+import Card from './Card';
+
+
+function Cardlist({robots}){
+  return (
+    <Fragment>
+    {
+      robots.map((user,i) =>{
+      return (
+        <Card 
+          key={i} 
+          id={user.id} 
+          name={user.name} 
+          email={user.email} 
+        />
+        );    
+      })
+    }
+    </Fragment>
+  );
+}
+
+export default Cardlist;
