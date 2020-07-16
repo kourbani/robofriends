e1da2fbb64f7e37ff53a6bd930ad20b5ad2a5f7f
kourbani <kanidoux@gmail.com>
2020-07-16 17:05:45 +0300

    added personalised message in searchbox

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -7,7 +7,7 @@ function Searchbox({searchChange}){
       <input
         className = "pa3 ba b--green bg-lightest-blue" 
         type="search" 
-        placeholder="Search Robots"
+        placeholder="Search Robots Kostaki"
         onChange = {searchChange}
         />
     </div>
